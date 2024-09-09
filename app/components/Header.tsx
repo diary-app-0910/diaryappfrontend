f1b52b8b14f4fb707ab527bf9b9ef833914f67ec
sudatema <dy.ren.ray@gmail.com>
2024-09-10 05:16:10 +0900

    プロジェクト作成とルーティング設定済み

---
+++ app/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <div>
+            Header nav
+            <nav>
+            <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/"
+                >
+            日記一覧
+            </Link>
+            <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/post"
+                >
+            日記入力
+            </Link>
+            <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/user"
+                >
+            アカウント情報
+            </Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        SignUP
+        
+        </main>
+    )
+}
+
+export default page
